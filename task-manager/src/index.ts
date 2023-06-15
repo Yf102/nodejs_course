d0c2f35d0121807ee07514b72e721c68f7b0e145
Filip Hristov <filip.hristov@glitnor.com>
2023-06-15 18:49:28 +0300

    Added dev and prod build

--- task-manager/src/index.ts
+++ task-manager/src/index.ts
@@ -2,8 +2,9 @@ import express from 'express'
 import { json } from 'body-parser'
 
 const app = express()
+const port = process.env.port || 3000
 app.use(json())
 
-app.listen (3000, () => {
-    console.log( 'server is listening on port 3000')
+app.listen (port, () => {
+    console.log( `server is listening on port ${port}`)
 })
