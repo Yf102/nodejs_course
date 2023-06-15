e64edbd0fbea17c4653335667f1ec6d917d40d51
Filip Hristov <filip.hristov@glitnor.com>
2023-06-15 19:33:35 +0300

    Added api calls for CRUD operations

--- task-manager/src/db/mongoose.ts
+++ task-manager/src/db/mongoose.ts
@@ -1,35 +1,17 @@
 import mongoose from "mongoose"
-import {IUser, UserModel, UserType} from "./models/user.model";
+import {IUser, UserModel, UserType} from "../models/user.model";
 import {type} from "os";
-import {ITask, TaskModel, TaskType} from "./models/task.model";
+import {ITask, TaskModel, TaskType} from "../models/task.model";
 
 const dbName = 'task-manager-api'
 const connectionUrl = `mongodb://127.0.0.1:27017/${dbName}`
 
-async function main() {
+const main = async () =>
     await mongoose.connect(connectionUrl)
 
-    // const desc: ITask = new TaskModel<TaskType>({
-    //     description: 'Test',
-    // })
-    //
-    // const _desc = await desc.save()
-    // console.log('_desc', _desc)
+export default main
 
-    // const me: IUser = new UserModel<UserType>({
-    //     name: 1,
-    //     email: 'VLADI@Gmail',
-    //     password: 'Test123',
-    //     age: 18
-    // })
-    //
-    // const _me = await me.save()
-    // console.log(_me)
-
-    return 'done...'
-}
-
-main()
-    .then((resp) => console.log(resp))
-    .catch(err => console.log(err))
-    .finally(() => mongoose.disconnect());
+// main()
+//     .then((resp) => console.log(resp))
+//     .catch(err => console.log(err))
+// .finally(() => mongoose.disconnect());
--- task-manager/src/index.ts
+++ task-manager/src/index.ts
@@ -1,10 +1,70 @@
 import express from 'express'
-import { json } from 'body-parser'
+import mongooseConnect from './db/mongoose'
+import {IUser, UserModel, UserType} from "./models/user.model";
+import mongoose from "mongoose";
 
 const app = express()
 const port = process.env.port || 3000
-app.use(json())
+app.use(express.json())
 
-app.listen (port, () => {
-    console.log( `server is listening on port ${port}`)
+
+app.get("/users", async (req, res) => {
+    await UserModel.find().then((allUsers) => {
+        res.status(200).json(allUsers)
+    }).catch((e) => {
+        res.status(400).json(e)
+    })
+
+});
+
+app.get("/users/:id", async (req, res) => {
+    const { id } = req.params;
+    await UserModel.findById(id).then((user) => {
+        res.status(200).json(user)
+    }).catch((e) => {
+        res.status(400).json(e)
+    })
+});
+
+app.post('/users', async (req, res) => {
+    const newUser: IUser = new UserModel<UserType>({ ...req.body })
+    await newUser.save().then((insertedUser) => {
+        res.status(201).json(insertedUser)
+    }).catch((e) => {
+        res.status(400).json(e)
+    })
 })
+
+app.put("/users/:id", async (req, res) => {
+    const { id } = req.params;
+    await UserModel.updateOne({ id }, req.body)
+    await UserModel.findById(id).then((updatedUser) => {
+        res.status(200).json(updatedUser)
+    }).catch((e) => {
+        res.status(400).json(e)
+    });
+});
+
+app.delete("/users/:id", async (req, res) => {
+    const { id } = req.params;
+    await UserModel.findByIdAndDelete(id).then((deletedUser) => {
+        res.status(200).json(deletedUser)
+    }).catch((e) => {
+        res.status(400).json(e)
+    })
+});
+
+const start = async () => {
+    try {
+        await mongooseConnect()
+            .then(resp => console.log(resp))
+            .catch(err => console.log(err))
+            // .finally(() => mongoose.disconnect())
+
+        app.listen(port, () => console.log(`Server started on port ${port}`));
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+}
+start()
