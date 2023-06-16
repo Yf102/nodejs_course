05901de9bc12d5daf37cdcb2e7cdbad0c8d03217
Filip Hristov <filip.hristov@glitnor.com>
2023-06-16 16:34:21 +0300

    More lint stuff

--- task-manager/src/routes/api/users.ts
+++ task-manager/src/routes/api/users.ts
@@ -1,17 +1,17 @@
-import { Router } from "express";
+import { Router } from 'express'
 import {
-    getAllUsers,
-    getUser,
-    createUser,
-    updateUser,
-    deleteUser
-} from "src/controllers/userController";
+  createUser,
+  deleteUser,
+  getAllUsers,
+  getUser,
+  updateUser,
+} from 'src/controllers/userController'
 
 const UserRouter = Router()
-    .get('', getAllUsers)
-    .get("/:id", getUser)
-    .post('/', createUser)
-    .put("/:id", updateUser)
-    .delete("/:id", deleteUser);
+  .get('', getAllUsers)
+  .get('/:id', getUser)
+  .post('/', createUser)
+  .put('/:id', updateUser)
+  .delete('/:id', deleteUser)
 
 export default UserRouter
