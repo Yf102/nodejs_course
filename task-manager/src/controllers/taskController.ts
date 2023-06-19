a01dff652c1fcbd8bf41952916de13f16e1bd088
Filip Hristov <filip.hristov@glitnor.com>
2023-06-19 10:46:30 +0300

    Converted all api calls to use async await methods

--- task-manager/src/controllers/taskController.ts
+++ task-manager/src/controllers/taskController.ts
@@ -2,39 +2,35 @@ import { Request, Response } from 'express'
 import { ITask, TaskModel, TaskType } from 'src/models/task.model'
 
 const getAllTasks = async (req: Request<{}, {}, {}>, res: Response) => {
-  await TaskModel.find()
-    .then((allTasks) => {
-      res.status(200).json(allTasks)
-    })
-    .catch((e) => {
-      res.status(500).json(e)
-    })
+  try {
+    const tasks = await TaskModel.find()
+    res.status(200).json(tasks)
+  } catch (e) {
+    res.status(500).json(e)
+  }
 }
 
 const getTask = async (req: Request, res: Response) => {
   const { id } = req.params
-  await TaskModel.findById(id)
-    .then((task) => {
-      if (!task) {
-        return res.status(404).json({ error: 'not found' })
-      }
-      res.status(200).json(task)
-    })
-    .catch((e) => {
-      res.status(500).json(e)
-    })
+  try {
+    const task = await TaskModel.findById(id)
+    if (!task) {
+      return res.status(404).json({ error: 'not found' })
+    }
+    res.status(200).json(task)
+  } catch (e) {
+    res.status(500).json(e)
+  }
 }
 
 const createTask = async (req: Request<{}, {}, TaskType>, res: Response) => {
   const newTask: ITask = new TaskModel<TaskType>({ ...req.body })
-  await newTask
-    .save()
-    .then((insertedTask) => {
-      res.status(201).json(insertedTask)
-    })
-    .catch((e) => {
-      res.status(400).json(e)
-    })
+  try {
+    const insertedTask = await newTask.save()
+    res.status(201).json(insertedTask)
+  } catch (e) {
+    res.status(400).json(e)
+  }
 }
 
 const updateTask = async (
@@ -42,14 +38,13 @@ const updateTask = async (
   res: Response
 ) => {
   const { id: _id } = req.params
-  await TaskModel.updateOne({ _id }, { ...req.body })
-  await TaskModel.findById(_id)
-    .then((updatedTask) => {
-      res.status(200).json(updatedTask)
-    })
-    .catch((e) => {
-      res.status(400).json(e)
-    })
+  try {
+    await TaskModel.updateOne({ _id }, { ...req.body })
+    const updatedTask = await TaskModel.findById(_id)
+    res.status(200).json(updatedTask)
+  } catch (e) {
+    res.status(400).json(e)
+  }
 }
 
 const deleteTask = async (
@@ -57,13 +52,12 @@ const deleteTask = async (
   res: Response
 ) => {
   const { id } = req.params
-  await TaskModel.findByIdAndDelete(id)
-    .then((deletedTask) => {
-      res.status(200).json(deletedTask)
-    })
-    .catch((e) => {
-      res.status(400).json(e)
-    })
+  try {
+    const deletedTask = await TaskModel.findByIdAndDelete(id)
+    res.status(200).json(deletedTask)
+  } catch (e) {
+    res.status(400).json(e)
+  }
 }
 
 export { createTask, deleteTask, getAllTasks, getTask, updateTask }
--- task-manager/src/controllers/userController.ts
+++ task-manager/src/controllers/userController.ts
@@ -2,40 +2,36 @@ import { Request, Response } from 'express'
 import { IUser, UserModel, UserType } from 'src/models/user.model'
 
 const getAllUsers = async (req: Request<{}, {}, {}>, res: Response) => {
-  await UserModel.find()
-    .then((allUsers) => {
-      res.status(200).json(allUsers)
-    })
-    .catch((e) => {
-      res.status(500).json(e)
-    })
+  try {
+    const users = await UserModel.find()
+    res.status(200).json(users)
+  } catch (e) {
+    res.status(500).json(e)
+  }
 }
 
 const getUser = async (req: Request, res: Response) => {
   const { id } = req.params
-  await UserModel.findById(id)
-    .then((user) => {
-      if (!user) {
-        return res.status(404).json({ error: 'not found' })
-      }
+  try {
+    const user = await UserModel.findById(id)
+    if (!user) {
+      return res.status(404).json({ error: 'not found' })
+    }
 
-      res.status(200).json(user)
-    })
-    .catch((e) => {
-      res.status(500).json(e)
-    })
+    res.status(200).json(user)
+  } catch (e) {
+    res.status(500).json(e)
+  }
 }
 
 const createUser = async (req: Request<{}, {}, UserType>, res: Response) => {
   const newUser: IUser = new UserModel<UserType>({ ...req.body })
-  await newUser
-    .save()
-    .then((insertedUser) => {
-      res.status(201).json(insertedUser)
-    })
-    .catch((e) => {
-      res.status(400).json(e)
-    })
+  try {
+    await newUser.save()
+    res.status(201).json(newUser)
+  } catch (e) {
+    res.status(400).json(e)
+  }
 }
 
 const updateUser = async (
@@ -43,14 +39,13 @@ const updateUser = async (
   res: Response
 ) => {
   const { id: _id } = req.params
-  await UserModel.updateOne({ _id }, req.body)
-  await UserModel.findById(_id)
-    .then((updatedUser) => {
-      res.status(200).json(updatedUser)
-    })
-    .catch((e) => {
-      res.status(400).json(e)
-    })
+  try {
+    await UserModel.updateOne({ _id }, req.body)
+    const updatedUser = await UserModel.findById(_id)
+    res.status(200).json(updatedUser)
+  } catch (e) {
+    res.status(400).json(e)
+  }
 }
 
 const deleteUser = async (
@@ -58,13 +53,12 @@ const deleteUser = async (
   res: Response
 ) => {
   const { id } = req.params
-  await UserModel.findByIdAndDelete(id)
-    .then((deletedUser) => {
-      res.status(200).json(deletedUser)
-    })
-    .catch((e) => {
-      res.status(400).json(e)
-    })
+  try {
+    const deletedUser = await UserModel.findByIdAndDelete(id)
+    res.status(200).json(deletedUser)
+  } catch (e) {
+    res.status(400).json(e)
+  }
 }
 
 export { createUser, deleteUser, getAllUsers, getUser, updateUser }
