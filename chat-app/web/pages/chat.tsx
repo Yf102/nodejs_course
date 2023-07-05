584284f17d75b181fc2878fa2f90760ccf93121f
Filip Hristov <filip.hristov@glitnor.com>
2023-07-05 18:15:52 +0300

    Added index page and chat page

---
+++ chat-app/web/pages/chat.tsx
@@ -0,0 +1,29 @@
+import styles from 'styles/Index.module.scss'
+import React, { useState } from 'react'
+import MessageForm, { RespType } from 'components/MessageForm'
+import TextMsg from 'components/TextMsg'
+import stylesIndex from 'styles/Index.module.scss'
+const Home = ({}) => {
+  const [receivedMessage, setReceivedMessage] = useState<RespType[]>([])
+  return (
+    <div data-testid='home-element' className={styles.container}>
+      <div className={stylesIndex['messages-form']}>
+        {receivedMessage.map((msg, index) => {
+          return <TextMsg type='mine' msg={msg} key={index} />
+        })}
+      </div>
+
+      <MessageForm onChange={(respMsg) => setReceivedMessage(respMsg)} />
+    </div>
+  )
+}
+
+export default Home
+
+// For SSR (performance gain with pre-rendering the page)
+export const getStaticProps = async () => {
+  return {
+    props: {},
+    revalidate: 86400,
+  }
+}
--- chat-app/web/pages/index.tsx
+++ chat-app/web/pages/index.tsx
@@ -1,24 +1,41 @@
+import React, { useRef } from 'react'
+import Input from 'components/FormElements/Input'
 import styles from 'styles/Index.module.scss'
-import React, { useState } from 'react'
-import MessageForm, { RespType } from 'components/MessageForm'
-import TextMsg from 'components/TextMsg'
-import stylesIndex from 'styles/Index.module.scss'
-const Home = ({}) => {
-  const [receivedMessage, setReceivedMessage] = useState<RespType[]>([])
-  return (
-    <div data-testid='home-element' className={styles.container}>
-      <div className={stylesIndex['messages-form']}>
-        {receivedMessage.map((msg, index) => {
-          return <TextMsg type='mine' msg={msg} key={index} />
-        })}
-      </div>
 
-      <MessageForm onChange={(respMsg) => setReceivedMessage(respMsg)} />
+const Index = () => {
+  const userNameRef = useRef<HTMLInputElement>(null)
+  const roomRef = useRef<HTMLInputElement>(null)
+  return (
+    <div className='flex flex-col max-w-sm mx-auto'>
+      <h1 className='font-bold text-3xl mb-10'>Join</h1>
+      <form className='flex flex-col items-center' action='/chat'>
+        <label>Display Name</label>
+        <Input
+          name='username'
+          type='text'
+          ref={userNameRef}
+          placeholder='Display Name'
+          required
+        />
+
+        <label>Room</label>
+        <Input
+          type='text'
+          name='room'
+          ref={roomRef}
+          placeholder='Room'
+          required
+        />
+
+        <button className={styles['join-button']} type='submit'>
+          Join
+        </button>
+      </form>
     </div>
   )
 }
 
-export default Home
+export default Index
 
 // For SSR (performance gain with pre-rendering the page)
 export const getStaticProps = async () => {
