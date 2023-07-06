7c0ef0c37f73ed9b988bd8f003bf438a45e51b43
Filip Hristov <filip.hristov@glitnor.com>
2023-07-06 15:27:22 +0300

    Added auto scroll

--- chat-app/web/pages/chat.tsx
+++ chat-app/web/pages/chat.tsx
@@ -1,15 +1,23 @@
 import MessageForm, { RespType } from 'components/MessageForm'
 import TextMsg from 'components/TextMsg'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import {
   default as styles,
   default as stylesIndex,
 } from 'styles/Index.module.scss'
 const Home = ({}) => {
   const [receivedMessage, setReceivedMessage] = useState<RespType[]>([])
+  const refScroll = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (refScroll.current) {
+      refScroll.current.scrollTop = refScroll.current.scrollHeight
+    }
+  }, [receivedMessage])
+
   return (
     <div data-testid='home-element' className={styles.container}>
-      <div className={stylesIndex['messages-form']}>
+      <div ref={refScroll} className={stylesIndex['messages-form']}>
         {receivedMessage.map((msg, index) => {
           return <TextMsg msg={msg} key={index} />
         })}
